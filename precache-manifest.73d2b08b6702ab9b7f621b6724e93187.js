3aaaee02d071343985d84bbc194dc0e2956a5b6d
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-04-16 23:57:21 +0200

    Updates

---
+++ precache-manifest.73d2b08b6702ab9b7f621b6724e93187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f8f91a5b0828a17597",
+    "url": "/Udacity-Final-MyReads/static/css/main.4f822fee.chunk.css"
+  },
+  {
+    "revision": "72f8f91a5b0828a17597",
+    "url": "/Udacity-Final-MyReads/static/js/main.35eec54b.chunk.js"
+  },
+  {
+    "revision": "7bcce8e9436433d9ec2b",
+    "url": "/Udacity-Final-MyReads/static/js/runtime~main.3ad3febc.js"
+  },
+  {
+    "revision": "4c88f67860a4693d521e",
+    "url": "/Udacity-Final-MyReads/static/js/2.a6c7ed47.chunk.js"
+  },
+  {
+    "revision": "be2d9c0ab05a2394bb2a83f06629bd4c",
+    "url": "/Udacity-Final-MyReads/static/media/switcher.be2d9c0a.svg"
+  },
+  {
+    "revision": "064aff6d9e4cdd8f528825b8aadbca37",
+    "url": "/Udacity-Final-MyReads/index.html"
+  }
+];
